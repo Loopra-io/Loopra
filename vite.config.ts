49d3b6f63fb76729c5881b0579afa9cdd2561824
Kener Alejandro Botina Bravo <kener.botina@correounivalle.edu.co>
2026-05-11 22:00:52 -0500

    Update base path in vite.config.ts
    Change base path in Vite configuration for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()], // Quitamos jsxLocPlugin para ganar velocidad
-  base: '/Loopra/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "src"),
