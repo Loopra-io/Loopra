0ef27ba8191a26662513170c9b7b216801a94803
kenerbotina <nse42250@gmail.com>
2026-05-15 23:19:53 -0500

    feat: estructura inicial loopra con vite 7 y react 19

--- src/App.tsx
+++ src/App.tsx
@@ -6,84 +6,35 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import Layout from './components/Layout';
 import ScrollToTop from './components/ScrollToTop';
 import { ThemeProvider } from "./contexts/ThemeContext";
+import ConsolaControl from './pages/ConsolaControl';
 import Documentacion from './pages/Documentacion';
 import Home from './pages/Home';
 import Proyectos from './pages/Proyectos';
 import SobreEstudio from './pages/SobreEstudio';
-import ConsolaControl from './pages/ConsolaControl';
 
 function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light" switchable>
         <TooltipProvider>
-          <Router>
+          {/* basename="/Loopra" para GitHub Pages: loopra-io.github.io/Loopra/ */}
+          <Router basename="/Loopra">
             <ScrollToTop />
             <Toaster />
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <Layout>
-                      <Home />
-                    </Layout>
-                  }
-                />
-                <Route
-                  path="/proyectos"
-                  element={
-                    <Layout>
-                      <Proyectos />
-                    </Layout>
-                  }
-                />
-                <Route
-                  path="/sobre-estudio"
-                  element={
-                    <Layout>
-                      <SobreEstudio />
-                    </Layout>
-                  }
-                />
-                <Route
-                  path="/documentacion"
-                  element={
-                    <Layout>
-                      <Documentacion />
-                    </Layout>
-                  }
-                />
-                <Route
-                  path="/consola-control"
-                  element={
-                    <Layout>
-                      <ConsolaControl />
-                    </Layout>
-                  }
-                />
-                <Route
-                  path="/404"
-                  element={
-                    <Layout>
-                      <NotFound />
-                    </Layout>
-                  }
-                />
-                {/* Final fallback route */}
-                <Route
-                  path="*"
-                  element={
-                    <Layout>
-                      <NotFound />
-                    </Layout>
-                  }
-                />
-              </Routes>
-            </Router>
-          </TooltipProvider>
-        </ThemeProvider>
-      </ErrorBoundary>
-    );
-  }
+            <Routes>
+              <Route path="/"              element={<Layout><Home /></Layout>} />
+              <Route path="/proyectos"     element={<Layout><Proyectos /></Layout>} />
+              <Route path="/sobre-estudio" element={<Layout><SobreEstudio /></Layout>} />
+              <Route path="/documentacion" element={<Layout><Documentacion /></Layout>} />
+              <Route path="/consola"       element={<Layout><ConsolaControl /></Layout>} />
+              <Route path="/404"           element={<Layout><NotFound /></Layout>} />
+              <Route path="*"              element={<Layout><NotFound /></Layout>} />
+            </Routes>
+          </Router>
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
 
 export default App;
