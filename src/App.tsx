2320d1e0bd3363fb5f0c9f460300f5860833f66e
Kener Alejandro Botina Bravo <kener.botina@correounivalle.edu.co>
2026-05-18 15:57:00 -0500

    fix: Cambiar basename de '/Loopra' a '/' para dominio personalizado loopra.me con TypeScript estricto y JSDoc

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,38 @@ import Home from './pages/Home';
 import Proyectos from './pages/Proyectos';
 import SobreEstudio from './pages/SobreEstudio';
 
-function App() {
+/**
+ * Componente principal de la aplicación Loopra.
+ *
+ * @remarks
+ * - Configurado con `basename="/"` para usar el dominio personalizado `https://loopra.me`
+ * - Si necesitas volver a GitHub Pages (loopra-io.github.io/Loopra/), cambia a `basename="/Loopra"`
+ * - React Router gestiona todas las rutas de la aplicación
+ * - El ErrorBoundary captura errores en componentes
+ * - ThemeProvider gestiona el tema light/dark
+ *
+ * @returns {JSX.Element} El árbol de componentes de la aplicación
+ */
+function App(): JSX.Element {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light" switchable>
         <TooltipProvider>
-          {/* basename="/Loopra" para GitHub Pages: loopra-io.github.io/Loopra/ */}
-          <Router basename="/Loopra">
+          {/* 
+            basename="/" para dominio personalizado (https://loopra.me)
+            Cambiar a basename="/Loopra" si vuelves a GitHub Pages standard
+          */}
+          <Router basename="/">
             <ScrollToTop />
             <Toaster />
             <Routes>
-              <Route path="/"              element={<Layout><Home /></Layout>} />
-              <Route path="/proyectos"     element={<Layout><Proyectos /></Layout>} />
+              <Route path="/" element={<Layout><Home /></Layout>} />
+              <Route path="/proyectos" element={<Layout><Proyectos /></Layout>} />
               <Route path="/sobre-estudio" element={<Layout><SobreEstudio /></Layout>} />
               <Route path="/documentacion" element={<Layout><Documentacion /></Layout>} />
-              <Route path="/consola"       element={<Layout><ConsolaControl /></Layout>} />
-              <Route path="/404"           element={<Layout><NotFound /></Layout>} />
-              <Route path="*"              element={<Layout><NotFound /></Layout>} />
+              <Route path="/consola" element={<Layout><ConsolaControl /></Layout>} />
+              <Route path="/404" element={<Layout><NotFound /></Layout>} />
+              <Route path="*" element={<Layout><NotFound /></Layout>} />
             </Routes>
           </Router>
         </TooltipProvider>
